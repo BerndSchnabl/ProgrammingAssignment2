0e70c079acb861eed2b08b758724e8b8b540fdde
Bernd Schnabl <bernd.schnabl@gmail.com>
2014-06-20 19:06:57 +0200

    Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,69 @@
 ## ####################################################################################
 ##
 ## makeCacheMatrix:
+## ================
 ## it has 2 pairs of getter / setter functions which are returned to the caller
 ## the get/set function pair for the original matrix (set and get)
 ## and the get/set function pair for the inverse matrix (setInverse/getInverse)
+## the function makeCacheMatrix returns a list of these 2 pairs of getter / setter
+## functions to the 
 ## 
 ## ####################################################################################
 makeCacheMatrix <- function(p_Matrix = matrix()) {
   
   m_invereMatrix <- NULL # local member variable hence no <<- operator
   
+  ## ####################################################################################
+  ##
+  ##
+  ##
+  ## ####################################################################################
   set <- function(p_Matrix) {
     g_Matrix <<- p_Matrix ## store the content in the global environment 
     g_Inverse <<- NULL ## reset global inverse martix since it is not yet computed
   }
   
+  ## ####################################################################################
+  ##
+  ##
+  ##
+  ## ####################################################################################
   get <- function() {
-    x ## this is the vector from the global environment and not the parameter x from the
+    g_Matrix ## this is the vector from the global environment
   }
   
-  setInverse <- function(mean) {
-    m <<- mean
+  ## ####################################################################################
+  ##
+  ##
+  ##
+  ## ####################################################################################
+  setInverse <- function(p_Inverse) {
+    g_Inverse <<- p_Inverse
   }
   
+  ## ####################################################################################
+  ##
+  ##
+  ##
+  ## ####################################################################################
   getInverse <- function() {
-    return(m)
+    return(g_inverse)
   }
   
+  ## ####################################################################################
+  ##
+  ## create the list of getter/setter function pairs.
+  ## the name of the element in the list (left side of assignment operation)
+  ## is the same as the setter / getter funciton name of the makeCacheMatrix
+  ## function (right side of assignment operation)
+  ##
+  ## ####################################################################################
   return_list <- list(set = set, get = get,  
                       setInverse = setInverse, 
                       getInverse = getInverse)
   
-  invisible(return_list)
+  invisible(return_list) ## return the getter/setter functions to caller
   
-  list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
